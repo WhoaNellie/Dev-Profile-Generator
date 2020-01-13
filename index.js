03b26b312c3d356f2ffa79c44f627a621fb6db61
Nellie <fabybacenelson@gmail.com>
2020-01-13 15:11:15 -0700

    requasted all data from github

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
+const convertFactory = require('electron-html-to');
 const axios = require("axios");
 const inquirer = require("inquirer");
 const generateHTML = require("./generateHTML.js");
-const toPDF = require('electron-html-to');
 
 function writeToFile(fileName, data) {
     console.log("write to file");
@@ -19,14 +19,39 @@ function init() {
             name: "color",
             choices: ["green", "blue", "pink", "red"]
         }
-    ]).then(function ({username, color}) {
-        const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+    ]).then(function ({
+        username,
+        color
+    }) {
+        const queryUrl = `https://api.github.com/users/${username}`;
+        const starQuery = `https://api.github.com/users/${username}/starred?per_page=1`;
+        
 
-        axios.get(queryUrl).then(function(res) {
-            console.log(res);
+        axios.get(queryUrl).then(function (res) {
+            const profPic = res.data.avatar_url;
+            const name = res.data.name;
+            const company = res.data.company;
+
+            const location = res.data.location;
+            const gitURL = res.data.html_url;
+            const blog = res.data.blog;
+            
+            const bio = res.data.bio;
+
+            const repos = res.data.public_repos;
+            const followers = res.data.followers;
+            const following = res.data.following;
+            
+        });
+
+        axios.get(starQuery).then(function (res) {
+            const stars = res.data.length;
+        });
+
+
     });
-});
 
+    // writeToFile();
 }
 
 init();
